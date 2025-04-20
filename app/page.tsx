305283cae1062e5580fca249e51dcb2c70637ecf
Saad Aslam <saadaslam13@Gmail.com>
2025-04-20 00:10:58 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new todo. saaad
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
